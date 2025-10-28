c14db8be646807c4a768553b185c8fa9d6d5479b
theopakieser <20101918@mail.wit.ie>
2025-10-28 13:44:56 +0000

    Commit Resolution

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-Fill in later
-=======
+
 # Welcome to your CDK TypeScript project
 
 This is a blank project for CDK development with TypeScript.
--- lib/cloud-app-ca-stack.ts
+++ lib/cloud-app-ca-stack.ts
@@ -1,18 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-<<<<<<< HEAD
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-=======
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
->>>>>>> b82b741cd5929876cf7b8b19cf247474cc096faa
 
 export class CloudAppCaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-<<<<<<< HEAD
     const moviesTable = new dynamodb.Table(this, 'MoviesTable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
@@ -22,18 +17,14 @@ export class CloudAppCaStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'getMovies.handler',
-      environment: {
-        TABLE_NAME: moviesTable.tableName,
-      },
+      environment: { TABLE_NAME: moviesTable.tableName },
     });
 
     const addMovieLambda = new lambda.Function(this, 'AddMovieLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'addMovie.handler',
-      environment: {
-        TABLE_NAME: moviesTable.tableName,
-      },
+      environment: { TABLE_NAME: moviesTable.tableName },
     });
 
     moviesTable.grantReadData(getMoviesLambda);
@@ -49,13 +40,5 @@ export class CloudAppCaStack extends cdk.Stack {
     movies.addMethod('POST', new apigateway.LambdaIntegration(addMovieLambda));
 
     new cdk.CfnOutput(this, 'ApiUrl', { value: api.url ?? 'No URL returned' });
-=======
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudAppCaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
->>>>>>> b82b741cd5929876cf7b8b19cf247474cc096faa
   }
 }

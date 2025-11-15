0d0166261a9cc75998097f6387eb5fcf18fea51a
theopakieser <20101918@mail.wit.ie>
2025-11-15 19:39:56 +0000

    removed all of auth - it broke three days ago and i have not been able to fix it

--- bin/cloud-app-ca.ts
+++ bin/cloud-app-ca.ts
@@ -1,17 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { CloudAppCaStack } from "../lib/cloud-app-ca-stack";
-import { CognitoStack } from "../lib/cognito-stack";
 
 const app = new cdk.App();
 
-
-const authStack = new CognitoStack(app, "AuthStack", {
-  env: { region: "us-east-1" },
-});
-
-
-new CloudAppCaStack(app, "CloudAppCaStack", {
-  env: { region: "us-east-1" },
-  authStack: authStack, 
-});
+new CloudAppCaStack(app, "CloudAppCaStack");
--- lib/cloud-app-ca-stack.ts
+++ lib/cloud-app-ca-stack.ts
@@ -1,20 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { CognitoStack } from "./cognito-stack";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
-interface CloudAppCaStackProps extends cdk.StackProps {
-  authStack: CognitoStack; 
-}
 
 export class CloudAppCaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: CloudAppCaStackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const { authStack } = props;
 
     const moviesTable = new dynamodb.Table(this, 'MoviesTable', {
       partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
@@ -118,75 +113,49 @@ const api = new apigateway.RestApi(this, "MoviesApi", {
   description: "API for managing movie information.",
 });
 
-const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, "MovieAuthorizer", {
-  cognitoUserPools: [authStack.userPool],
-});
+
 
 // /movies
 const movies = api.root.addResource("movies");
 movies.addMethod("GET", new apigateway.LambdaIntegration(getMoviesLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
 });
 
 movies.addMethod("POST", new apigateway.LambdaIntegration(addMovieLambda), {
-  apiKeyRequired: true,
 });
 
 
 // /movies/{id}
 const movieById = movies.addResource("{id}");
 movieById.addMethod("GET", new apigateway.LambdaIntegration(getMovieByIdLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
 });
 movieById.addMethod("DELETE", new apigateway.LambdaIntegration(deleteMovieLambda), {
-  apiKeyRequired: true,
 });
 
 // /actors
 const actors = api.root.addResource("actors");
 actors.addMethod("GET", new apigateway.LambdaIntegration(getActorsLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
+  
 });
 actors.addMethod("POST", new apigateway.LambdaIntegration(addActorsLambda), {
-  apiKeyRequired: true,
 });
 
 // /awards
 const awards = api.root.addResource("awards");
 awards.addMethod("GET", new apigateway.LambdaIntegration(getAwardsLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
+ 
 });
 
 // /movies/{id}/actors
 const actorsInMovie = movieById.addResource("actors");
 actorsInMovie.addMethod("GET", new apigateway.LambdaIntegration(getCastByMovieLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
+ 
 });
 
 // /movies/{id}/actors/{actorId}
 const specificActor = actorsInMovie.addResource("{actorId}");
 specificActor.addMethod("GET", new apigateway.LambdaIntegration(getActorInMovieLambda), {
-  authorizer,
-  authorizationType: apigateway.AuthorizationType.COGNITO,
+ 
 });
-
-// cognito admin api
-const plan = api.addUsagePlan("UsagePlan", {
-  name: "AdminUsagePlan",
-  throttle: { rateLimit: 10, burstLimit: 2 },
-});
-
-const apiKey = api.addApiKey("AdminApiKey", {
-  apiKeyName: "MovieAdminKey",
-});
-
-plan.addApiKey(apiKey);
-plan.addApiStage({ stage: api.deploymentStage });
 
 
 // Output the API URL
